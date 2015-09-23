d380cc18369c0144c1640e70c6c532d9df5b02a2
Rob <wimnat@users.noreply.github.com>
2015-09-23 06:44:37 +0000

    Update ec2_facts_windows.ps1

--- cloud/amazon/ec2_facts_windows.ps1
+++ cloud/amazon/ec2_facts_windows.ps1
@@ -24,6 +24,7 @@ $ec2_sshdata_uri  = 'http://169.254.169.254/latest/meta-data/public-keys/0/opens
 $ec2_userdata_uri = 'http://169.254.169.254/latest/user-data/'
 $global:data = @{}
 $prefix = 'ansible_ec2'
+$filter_patterns = @('public-keys')
 
 # result
 $result = New-Object psobject @{
@@ -32,47 +33,121 @@ $result = New-Object psobject @{
 
 Function _Fetch($url) {
 
-	Invoke-RestMethod -Uri $url
-    #Write-Host ($rest_result | Out-String)
-	#if ($rest_result.code -eq 200) {
-#		$result.Add('bla', $rest_result.message)
-#	} else {
-#		$result.Add('bla', $rest_result.message)
-#    }
-    #return
+    try {
+	    Invoke-RestMethod -Uri $url
+        return
+    } catch [System.Net.WebException] {
+        return $null
+    }
 	
 }
 
-Function Fetch($uri) {
+$global:temp_data = @{}
 
+Function Fetch($uri, $_data=@{}) {
+
+    $sg_fields = @()
     $raw_subfields = (_Fetch $uri)
-    Write-Host ($raw_subfields | Out-String)
-    $subfields = ($raw_subfields -split '[\n]') |? {$_}
 
-    Write-Host $subfields
+    if (!$raw_subfields) {
+        return
+    }
+    
+    $subfields = ($raw_subfields -split '[\n]') |? {$_}
 
     foreach ($field in $subfields) {
         If ($field.EndsWith("/")) {
-            Fetch($uri + $field)
-            #Write-Host $field
-        } 
+            #Set-Variable -Name $global:var1 -Value (Fetch($uri + $field)) -Scope Global
+            #$_data = $data + (Fetch($uri + $field))
+            $x = Fetch($uri + $field)
+            try {
+                $global:temp_data =  $global:temp_data + $x
+            } catch [System.ArgumentException] {
+                
+            }
+        }
         
         If ($uri.EndsWith("/")) {
             $new_uri = $uri + $field
         } Else {
             $new_uri = $uri + "/" + $field
-            #$global:data.Add($field, _Fetch($uri + $field))
-            #Write-Host $field
-            #Write-Host (_Fetch($uri + $field) | Out-String)
         }
-        Write-Host $new_uri
-        If (-Not ($new_uri.EndsWith("/"))) {
-            Write-Host (_Fetch($new_uri) | Out-String)
+
+        If (($new_uri -notlike "*=*") -or (-Not ($new_uri.EndsWith("/")))) {
+            $content = _Fetch($new_uri)
+            if ($field -eq "security-groups") {
+                $sg_fields += ($content -split '[\n]') -join ","
+                $_data[$new_uri] = $sg_fields
+            } Else {
+                $_data[$new_uri] = $content
+            }
         }
     }
 
+    return $_data + $global:temp_data
+
 }
 
-Write-Host (Fetch($ec2_metadata_uri))
+Function Mangle_Fields([Hashtable]$fieldsxx, $uri) {
+    
+    Write-Host $fieldsxx.gettype()
+    #Write-Host ($fields | Out-String)
+    $new_fields = @{}
+    #foreach ($field in $fields.GetEnumerator()) {
+    $fields.GetEnumerator() | % {
+        Write-Host "$($_.key)"
+    }
+}
 
-#Write-Host ($result | Out-String)
+
+$_data = Fetch($ec2_metadata_uri)
+
+$new_fields = @{}
+
+# Add Ansible variable prefix
+foreach ($field in $_data.GetEnumerator()) {
+    #Write-Host "$($field.Name): $($field.Value)"
+    $split_fields = $field.Name.Remove(0,$ec2_metadata_uri.length)
+    $new_key = "-" + $split_fields
+    $new_fields[$prefix + $new_key] = $field.Value
+}
+
+# Remove public keys meta
+$remove_list = @()
+foreach ($pattern in $filter_patterns) {
+    foreach ($key in $new_fields.Keys) {
+        Write-Output $key
+        Write-Output $pattern
+        if ($key -like "*" + $pattern + "*") {
+            $remove_list += $key
+        }
+    }
+}
+foreach ($item in $remove_list) {
+    $new_fields.Remove($item)
+}
+
+$final_fields = @{}
+# Fix invalid var names
+foreach ($field in $new_fields.GetEnumerator()) {
+    $new_field = $field.Name -replace ":", "_"
+    $new_field = $new_field -replace "-", "_"
+    $final_fields[$new_field] = $field.Value
+    #$new_fields.Remove($field)
+    Write-Host $new_field
+    #Write-Host "$($field.Name): $($field.Value)"
+}
+
+Exit-Json $final_fields
+
+#Write-Host ($final_fields | Format-Table -Wrap | Out-String)
+
+#Write-Host $_data.GetType()
+
+#Mangle_Fields([Hashtable]$_data, $ec2_metadata_uri)
+
+
+#Write-Host ($_data | Format-Table -Wrap | Out-String)
+
+#Write-Host $global:var1.GetType()
+#Write-Host ($global:var1 | Format-Table -Wrap | Out-String)
